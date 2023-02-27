7ce6504af998915c8fd186d324cf90044e76747c
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-02-27 21:51:06 +0530

    Update typingword.py

--- typingword.py
+++ typingword.py
@@ -1,6 +1,6 @@
 import random
 
-# Define a list of 100 random words
+
 words = ['apple', 'banana', 'orange', 'cherry', 'grape', 'kiwi', 'lemon', 'mango', 'peach', 'pear',
          'pineapple', 'strawberry', 'watermelon', 'blueberry', 'raspberry', 'blackberry', 'apricot',
          'avocado', 'cantaloupe', 'coconut', 'fig', 'guava', 'honeydew', 'lime', 'papaya', 'plum',
@@ -13,20 +13,20 @@ words = ['apple', 'banana', 'orange', 'cherry', 'grape', 'kiwi', 'lemon', 'mango
          'crab', 'clam', 'oyster', 'mussels', 'pizza', 'burger', 'hotdog', 'taco', 'burrito',
          'sushi', 'ramen', 'pho', 'padthai', 'curry', 'stirfry', 'salmon', 'trout', 'tuna']
 
-# Shuffle the list of words
+
 random.shuffle(words)
 
-# Define initial score and maximum number of attempts
+
 score = 0
 max_attempts = 5
 
-# Loop through each word in the shuffled list
+
 for word in words:
     attempts = 0
     while attempts < max_attempts:
-        # Get user input
+       
         user_input = input(f"Type the word '{word}': ")
-        # Check if user input matches the word
+        
         if user_input == word:
             print("Correct!")
             score += 1
@@ -38,5 +38,5 @@ for word in words:
     else:
         print(f"Sorry, the word was '{word}'.")
     
-# Print final score
+
 print(f"Your final score is {score}.")
